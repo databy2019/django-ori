f53890ee1872130cea3072911d8d4d57ae48b136
databy2019 <databy2019@gmail.com>
2021-06-21 15:35:57 +0700

    update settings.py

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = 'django1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'db_okuselatan2020_1',
+        'HOST': 'database-1.cag2d2oxfxfn.us-east-2.rds.amazonaws.com',
+        'USER': 'postgres',
+        'PASSWORD':'xert2101',
+        'PORT':'5431',
     }
 }
 
